87c17974872f20e6b842f30c7da9510dcd8647d4
jayrskie <senaganbenignojr@gmail.com>
2025-12-12 06:18:00 +0800

    small changes

--- admin/admin-view-feedback.php
+++ admin/admin-view-feedback.php
@@ -14,9 +14,8 @@ if (!isset($_SESSION['library_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Transactions/Reports</title>
+    <title>Feedback</title>
     <link rel="stylesheet" href="../dashboard-style.css">
-    <link rel="stylesheet" href="view-transaction-style.css">
 </head>
 <body>
     <div class="sidebar">
@@ -28,20 +27,14 @@ if (!isset($_SESSION['library_id'])) {
                 <li><a href="admin-dashboard.php"><img class="nav-icon" src="../images/house.png" alt="Overview"> <span class="nav-text">Dashboard</span></a></li>
                 <li><a href="admin-manage-books.php"><img class="nav-icon" src="../images/books.png" alt="Books"> <span class="nav-text">Manage Books</span></a></li>
                 <li><a href="admin-manage-users.php"><img class="nav-icon" src="../images/calendar.png" alt="Users"> <span class="nav-text">Manage Users</span></a></li>
-                <li class="clicked-page"><a href="admin-view-transactions.php"><img class="nav-icon" src="../images/return.png" alt="Return"> <span class="nav-text">View Transactions</span></a></li>
-                <li><a href="admin-view-feedback.php"><img class="nav-icon" src="../images/people.png" alt="Users"> <span class="nav-text">View Feedback</span></a></li>
+                <li><a href="admin-view-transactions.php"><img class="nav-icon" src="../images/return.png" alt="Return"> <span class="nav-text">View Transactions</span></a></li>
+                <li class="clicked-page"><a href="admin-view-feedback.php"><img class="nav-icon" src="../images/people.png" alt="Users"> <span class="nav-text">View Feedback</span></a></li>
                 <li><a href="../logout.php" class="logout-btn"><img class="nav-icon" src="../images/logout.png" alt="Logout"> <span class="nav-text">Logout</span></a></li>
             </ul>
         </nav>
     </div>
     <div class="main-content">
         <div class="header">
-            <div class="header-actions">
-                <a href="admin-view-transactions.php" class="header-btn clicked-header-page">Borrow Request</a>
-                <a href="admin-borrowed-books.php" class="header-btn">Borrowed Books</a>
-                <a href="admin-borrow-history.php" class="header-btn">Borrow History</a>
-                <a href="admin-return-history.php" class="header-btn">Return History</a>
-            </div>
             <span class="welcome-text">Welcome, <?php echo $displayName; ?></span>
         </div>
     </div>
